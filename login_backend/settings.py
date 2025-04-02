547b9e731e7335ac029dc772e9f80e95ae0d3f2d
kaustubhkharat <kakharat15@gmail.com>
2025-04-02 15:46:12 +0530

    Implemented sugested changes for request dialog and email check

--- login_backend/settings.py
+++ login_backend/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -110,9 +109,9 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'venue_booking2',
-        'USER': 'root',
-        'PASSWORD': 'Arnav@123',
+        'NAME': 'venue_booking',
+        'USER': 'kaustubh',
+        'PASSWORD': 'KKOct#@20',
         'HOST': 'localhost',  # Change if using a remote server
         'PORT': '3306',  # MySQL default port
     }
