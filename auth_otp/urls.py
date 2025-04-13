fa2bb70a1cc8c643f8ab1211dad6125745d23af6
kaustubhkharat <kakharat15@gmail.com>
2025-04-13 13:45:49 +0530

    added all fields to be displayed to the user

--- auth_otp/urls.py
+++ auth_otp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name='auth_otp'
+
 urlpatterns = [
     path('forgot-password/', views.forgot_password, name='forgot_password'),
     path('verify-otp/', views.verify_otp, name='verify_otp'),
