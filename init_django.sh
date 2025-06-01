8c552fa8a49d5b0b92afb0c225e9356e4516f695
Abhishek Shinde <abhishinde889@gmail.com>
2025-06-01 14:32:05 +0530

    minor changes in user_info and init script

--- init_django.sh
+++ init_django.sh
@@ -9,11 +9,7 @@ python3 manage.py migrate
 echo "Importing users..."
 python3 manage.py import_users user_info.xlsx
 
-echo "Importing buildings..."
-python3 manage.py import_buildings Building_info.csv
-
 echo "Importing venues..."
 python3 manage.py import_venues Data1.xlsx
 
 echo "All done!"
-
