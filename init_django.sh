97de4588ac001889551539d60be58efff865d25f
Arnav Prasad <arnavprasad@Arnavs-MacBook-Pro.local>
2025-07-27 23:15:34 +0530

    updated init_django.sh file

--- init_django.sh
+++ init_django.sh
@@ -6,10 +6,10 @@ python3 manage.py makemigrations
 echo "Running migrate..."
 python3 manage.py migrate
 
-echo "Importing users..."
-python3 manage.py import_users user_info.xlsx
+# echo "Importing users..."
+# python3 manage.py import_users user_info.xlsx
 
-echo "Importing venues..."
-python3 manage.py import_venues Venues_Data.xlsx
+# echo "Importing venues..."
+# python3 manage.py import_venues Venues_Data.xlsx
 
 echo "All done!"
